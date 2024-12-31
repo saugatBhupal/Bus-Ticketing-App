729d74039cf8a43da5693d9149fdc3879a81e388
Saugat <saugatbhupal@bhunet.com>
2024-12-31 10:17:47 +0545

    simple interest cal

--- lib/test.dart
+++ lib/test.dart
@@ -1,67 +1,10 @@
 import 'package:flutter/material.dart';
 
-class SimpleInterestView extends StatelessWidget {
-  final TextEditingController _principalController = TextEditingController();
-
-  final TextEditingController _rateController = TextEditingController();
-
-  final TextEditingController _timeController = TextEditingController();
+class Test extends StatelessWidget {
+  const Test({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Simple Interest Calculator"),
-        centerTitle: true,
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            TextField(
-              controller: _principalController,
-              keyboardType: TextInputType.number,
-              decoration: const InputDecoration(
-                labelText: "Principal Amount",
-                border: OutlineInputBorder(),
-              ),
-            ),
-            const SizedBox(height: 16.0),
-            TextField(
-              controller: _rateController,
-              keyboardType: TextInputType.number,
-              decoration: const InputDecoration(
-                labelText: "Rate of Interest (%)",
-                border: OutlineInputBorder(),
-              ),
-            ),
-            const SizedBox(height: 16.0),
-            TextField(
-              controller: _timeController,
-              keyboardType: TextInputType.number,
-              decoration: const InputDecoration(
-                labelText: "Time (Years)",
-                border: OutlineInputBorder(),
-              ),
-            ),
-            const SizedBox(height: 24.0),
-            ElevatedButton(
-              onPressed: () {},
-              child: const Text("Calculate"),
-            ),
-            const SizedBox(height: 24.0),
-            const Text(
-              "45",
-              style: TextStyle(
-                fontSize: 18.0,
-                fontWeight: FontWeight.bold,
-              ),
-              textAlign: TextAlign.center,
-            ),
-          ],
-        ),
-      ),
-    );
+    return const Placeholder();
   }
 }
